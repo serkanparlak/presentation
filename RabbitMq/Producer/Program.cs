b00e2594ca9b03aec425633519bf54e003852d27
Serkan Parlak <@GTUSR0306>
2022-07-25 23:34:51 +0300

    initial commit

---
+++ RabbitMq/Producer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "hello",
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null);
+
+    string message = "Hello World!";
+    var count = 0;
+    Console.WriteLine("Producing.");
+
+
+    while (true)
+    {
+        var body = Encoding.UTF8.GetBytes(message + "-" + count++);
+        // produce
+        channel.BasicPublish(exchange: "",
+                             routingKey: "hello",
+                             basicProperties: null,
+                             body: body);
+
+        System.Threading.Thread.Sleep(10);
+    }
+}
